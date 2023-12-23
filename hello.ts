6bdcd15ee0018d7e5838cae7190ef40145a9e90b
ren-yamanashi <pinokionigg@icloud.com>
2023-12-23 21:56:19 +0900

    Hello Hono! -> Hello World! #5

--- hello.ts
+++ hello.ts
@@ -3,6 +3,6 @@ import { Hono } from "https://deno.land/x/hono/mod.ts";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello! Hono!"));
+app.get("/", (c) => c.text("Hello! World!"));
 
 serve(app.fetch, { port: 1993 });
